1dddea4f580c474dda5ae671813da7afc9da5fd2
Luke Caitlin <162427264+mannanchovi@users.noreply.github.com>
2024-03-06 10:45:07 +0200

    Create new.js

---
+++ new.js
@@ -0,0 +1,29 @@
+return new Promise((resolve, reject) => {
+			var column = this._addColumn(definition, before, nextToColumn);
+			
+			this._reIndexColumns();
+			
+			this.dispatch("column-add", definition, before, nextToColumn);
+			
+			if(this.layoutMode() != "fitColumns"){
+				column.reinitializeWidth();
+			}
+			
+			this.redraw(true);
+			
+			this.table.rowManager.reinitialize();
+			
+			this.rerenderColumns();
+			
+			resolve(column);
+		});
+	}
+
+if(!this.redrawBlock){
+			this.renderer.rerenderColumns(update, silent);
+		}else{
+			if(update === false || (update === true && this.redrawBlockUpdate === null)){
+				this.redrawBlockUpdate = update;
+			}
+		}
+	}
